35ce7481406d7c704d1ac0dc7cd7757f09e7740b
dilansci <dylan.branda1@gmail.com>
2025-07-25 10:33:55 +0100

    Initial jquery logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,23 @@
-// Logic for webpage. jQuery should be used here
+$(document).ready(function() {
+    alert("Welcome to the CW2 website! Fill out our form!");
+
+    $('#contactForm').submit(function(event) {
+        event.preventDefault();
+
+        var name = $('#name').val();
+        var phone = $('#phone').val();
+        var email = $('#email').val();
+        var address = $('#address').val();
+
+        console.log("Form submitted with the following data:");
+        console.log("Name: " + name);
+        console.log("Phone: " + phone);
+        console.log("Email: " + email);
+        console.log("Address: " + address);
+
+        alert("Thank you for your message, " + name + "! We will get back to you soon.");
+        
+        // Reset the form fields
+        $(this).trigger("reset");
+    });
+});
