9b9795bed4f5ee657dbed35b2f10ec420880deb6
gerrydeemo <gerrydeemo@gmail.com>
2025-08-04 01:16:11 +0100

    change to navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,20 @@
-class MyNavbar extends HTMLElement {
-    connectedCallback() {
-      console.log('<my-nav> connected');
-      $.get('navbar.html', (data) => {
-        this.innerHTML = data;
-        console.log('nav loaded');
-      });
-    }
-}
-
-customElements.define('my-navbar', MyNavbar);
+class MyNavbar extends HTMLElement { 
+		connectedCallback() { 
+				console.log('<my-nav> connected'); 
+				$.get('navbar.html', (data) => { 
+						this.innerHTML = data; 
+						console.log('nav loaded');  
+						const menuBtn = this.querySelector('#menu-btn'); 
+						const mobileMenu = this.querySelector('#mobile-menu'); 
+						if (menuBtn && mobileMenu) { 
+								menuBtn.addEventListener('click', () => { 
+										mobileMenu.classList.toggle('visible'); 
+								}); this.querySelectorAll('.ja-mobile-menu-link').forEach(link => {
+										link.addEventListener('click', () => { 
+												mobileMenu.classList.remove('visible'); 
+										}); 
+								}); 
+						} 
+				}); 
+		} 
+} customElements.define('my-navbar', MyNavbar);
